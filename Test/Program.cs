9ecc2bd8857f864c97a30ea6521a78ab8e7c5edf
TheEmeraldBee <brightonlcox@gmail.com>
2022-11-11 08:07:57 -0700

    Initial Commit with functional language

---
+++ Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using MiniLang;
+
+var projects = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/MiniLangProjects/";
+if (!Directory.Exists(projects))
+{
+    Console.Error.WriteLine("ERROR: Could Not Find MiniLangProjects folder in Documents. Please Create It!");
+    return;
+}
+
+Console.WriteLine("Please Enter Your Project Name\n(Should be a .minilang file in your documents/MiniLangProjects folder.): ");
+var file = Console.ReadLine();
+if (!File.Exists(projects + file + ".minilang"))
+{
+    Console.Error.WriteLine($"ERROR: Could Not Find Project {file}.minilang in your projects. Please Create It or Fix The Name!");
+    return;
+}
+
+var engine = new Engine();
+engine.HandleCodeFile(projects + file + ".minilang");
+// engine.HandleCodeFile();
+
+// while (true)
+// {
+//     Console.Write("Mini Lang::: ");
+//     var res = Console.ReadLine() ?? string.Empty;
+//     
+//     engine.HandleCodeString(res);
+//     
+//     Console.WriteLine();
+// }
