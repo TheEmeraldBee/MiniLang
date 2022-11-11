80b57fa0163afb9a3a409a0298c3e2ed2a82de1d
TheEmeraldBee <brightonlcox@gmail.com>
2022-11-11 16:23:10 -0700

    Added console project and changed test to run text for people to edit.

---
+++ MiniLangConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using MiniLang.Core;
+using MiniLang.Internal;
+
+var engine = new Engine(new DefaultWriter());
+engine.AddLibrary(new DefaultLibrary());
+
+while (true)
+{
+    Console.Write("MiniLang ::: ");
+    engine.Run(Console.ReadLine() ?? string.Empty);
+    Console.WriteLine();
+}
